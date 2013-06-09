64af3f5f071330be00bc3c98ce3063d624cd235d
SHIRACHA rikyu <shiracha.rikyu@gmail.com>
2013-06-09 13:48:22 +0900

    とりあえず厳密なソルバにした

--- main.cpp
+++ main.cpp
@@ -2,44 +2,55 @@
 #include <iostream>
 
 
-// 5桁同士の積には十分な型ならいいのでより小さくできる。16bit環境は考慮していない
-// ただしnの指定もこれなのでそこが問題になるのに注意
-typedef unsigned long long natural;
+// 5桁同士の積には十分な型ならいいのでより小さくできうるが
+// ただしnの指定もこれなのでそこが問題になるのでもう大きな型でいいかな。という妥協
+typedef std::uint64_t natural;
+
+// 対象にする桁数
+static const natural digit = 100000ll;
+
+natural cut( natural val )
+{
+	// 下位の0になっている桁は全て演算にも答えにも寄与しないのでカットしてよい
+	while( (val % 10) == 0 ) val /= 10;
+
+	// 10進数で考えるので5*2とか5*4とか5*8といったものがあり
+	// 桁間の和分で流れる可能性が結局ある。
+	// そのため上位桁を考慮分と同等だけ残す
+	// 直感的にそれ以上は考慮しなくて良いはず……(未証明)
+	// 2進数であれば0が最下位桁に来ないinvariantが効いてくるのになー。
+	static const natural need_think_digit = digit*digit;
+	val %= need_think_digit;
+
+	return val;
+}
 
 natural f( natural n )
 {
 	natural val = 1;
-
 	for( natural i = 1; i <= n; ++i )
 	{
-		if( (i % 10000000) == 0 )
-		{
-			std::cout << "calc now: " << i << std::endl;
-		}
-		natural mul = i;
-		while( (mul % 10) == 0 ) mul /= 10;
-		mul %= 1000000;
-
-		val *= mul;
-
-		while( (val % 10) == 0 ) val /= 10;
-		val %= 1000000;	// オーバフロー対策に周回ごとに考慮の必要な下部5桁に絞る
+		val *= cut( i );
+		val = cut( val );
 	}
 
-	return val % 100000;
+	// 対象桁数までカット
+	return val % digit;
 }
 
 int main(void)
 {
-	std::cout << f(99999) << std::endl;
-/*
-	std::cout << f(27) << std::endl;
-	std::cout << f(1018) << std::endl;
-	std::cout << f(4318285) << std::endl;
+	std::cout << "f(99999) = " << f(99999ll) << std::endl;
+	std::cout << "f(27) = " << f(27ll) << std::endl;
+	std::cout << "f(1018) = " << f(1018ll) << std::endl;
+	std::cout << "f(4318285) = " << f(4318285ll) << std::endl;
 
-	std::cout << f(1000000000000) << std::endl;
-	std::cout << f(5638029384213847) << std::endl;
-*/
+	// これだけではhardは計算が終わりません
+	// おそらく積の結合性を使うと解けるようになりますが、とりあえずここまで。
+	/*
+	std::cout << "f(1000000000000) = " << f(1000000000000ll) << std::endl;
+	std::cout << "f(5638029384213847) = " << f(5638029384213847ll) << std::endl;
+	*/
 
 	return 0;
 }
